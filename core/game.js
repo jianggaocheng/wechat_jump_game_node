f9f23c639f449cf2cc748fbf855e150663619576
jianggc <jianggaocheng@hotmail.com>
2018-01-12 15:37:26 +0800

    update

--- core/game.js
+++ core/game.js
@@ -1,7 +1,67 @@
-const sharp = require('sharp');
+const Jimp = require("jimp");
+
+// 计算棋子底部中心坐标
+async let getChessPos = im => {
+  let scanEndX = im.bitmap.width;
+  let scanEndY = im.bitmap.height * 2 / 3;
+
+  // 棋子的左边缘坐标
+  let minChessX = im.bitmap.width;
+  let minChessY = 0;
+
+  // 棋子的右边缘坐标
+  let maxChessX = 0;
+  let maxChessY = 0;
+
+  // 根据左右边缘坐标算出棋子底部中心坐标
+  let chessX = 0;
+  let chessY = 0;
+
+  return new Promise((resolve, reject) => {
+    im.scan(0, deviceConfig.scanStartY, scanEndX, scanEndY, function(x, y, idx) {
+      let red = this.bitmap.data[idx + 0];
+      let green = this.bitmap.data[idx + 1];
+      let blue = this.bitmap.data[idx + 2];
+  
+      if (
+        40 < red &&
+        red < 66 &&
+        (30 < green && green < 63) &&
+        (68 < blue && blue < 110)
+      ) {
+        if (x < minChessX) {
+          minChessX = x;
+          minChessY = y;
+        }
+  
+        if (x == minChessX) {
+          minChessY = (minChessY + y) / 2;
+        }
+  
+        if (x > maxChessX) {
+          maxChessX = x;
+          maxChessY = (maxChessY + y) / 2;
+        }
+  
+        // 颜色匹配测试
+        // im.setPixelColor(0xFFFFFFFF, x, y);
+      }
+  
+      if (x == scanEndX - 1 && y == scanEndY - 1) {
+        // image scan finished, do your stuff
+        chessX = Math.round((minChessX + maxChessX) / 2);
+        chessY = Math.round((minChessY + maxChessY) / 2);
+  
+        logger.debug("Chess scan finish", chessX, chessY);
+  
+        resolve({x: chessX, y: chessY});
+      }
+    });
+  });
+};
 
 module.exports = {
-    parseScreenShot: function(image, callback) {
-        
-    },
-}
+  autoPlay: async function() {
+    return new Promise((resolve, reject) => {});
+  }
+};
--- iOS_auto.js
+++ iOS_auto.js
@@ -5,55 +5,65 @@ const moment = require("moment");
 const async = require("async");
 const Jimp = require("jimp");
 const logger = require("./core/logger");
+const game = require("./core/game");
 const config = require("./config/index");
 const wdaServerURL = "http://192.168.1.15:8100";
 
-const deviceConfig = require("./config.json");
+global deviceConfig = require("./config.json");
 
 // disable wda console output
 console.log = function() {};
 
-// let client = wda.client(wdaServerURL);
+let client = wda.client(wdaServerURL);
 
-// let status = client.status;
-// logger.debug(
-//   "Connect",
-//   status.value.ios.ip,
-//   status.value.os.name + status.value.os.version
-// );
+const wdaDriver = require('wda-driver');
 
-// let screenShotDir = path.resolve(
-//   config.screenShotPath,
-//   moment().format("YYYYMMDD-HHmmss")
-// );
-// fs.mkdirSync(screenShotDir);
+const c = new wdaDriver.Client(wdaServerURL);
+
+let status = client.status;
+logger.debug(
+  "Connect",
+  status.value.ios.ip,
+  status.value.os.name + status.value.os.version
+);
+
+let screenShotDir = path.resolve(
+  config.screenShotPath,
+  moment().format("YYYYMMDD-HHmmss")
+);
+fs.mkdirSync(screenShotDir);
 
 let takeScreenShot = function(callback) {
   // fake
   // return callback(null, "./test.png");
 
-  // let screenshot = client.screenshot().value;
+  let screenshot = client.screenshot().value;
 
-  // let dataBuffer = new Buffer(screenshot, "base64");
-  // let filename = moment().format("HHmmssSSS") + ".png";
+  let dataBuffer = new Buffer(screenshot, "base64");
+  let filename = moment().format("HHmmssSSS") + ".png";
 
-  // fs.writeFile(screenShotDir + "/" + filename, dataBuffer, function(err) {
-  //   if (err) {
-  //     callback(err);
-  //   } else {
-  //     callback(null);
-  //   }
-  // });
-
-  Jimp.read("./test.png", function(err, im) {
-    logger.debug(`Get screen shot: ${im.bitmap.height} * ${im.bitmap.width}`);
-
-    callback(err, im);
+  fs.writeFile(screenShotDir + "/" + filename, dataBuffer, function(err) {
+    if (err) {
+      callback(err);
+    } else {
+      Jimp.read(dataBuffer, function(err, im) {
+        logger.debug(`Get screen shot: ${im.bitmap.height} * ${im.bitmap.width}`);
+    
+        callback(err, im);
+      });
+    }
   });
+
+  // fake
+  // Jimp.read("./test.png", function(err, im) {
+  //   logger.debug(`Get screen shot: ${im.bitmap.height} * ${im.bitmap.width}`);
+
+  //   callback(err, im);
+  // });
 };
 
 let getChessPos = function(result, callback) {
-  let im = result.takeScreenShot;
+  let im = result.screenShot;
 
   let scanEndX = im.bitmap.width - 300;
   let scanEndY = im.bitmap.height * 2 / 3;
@@ -100,30 +110,31 @@ let getChessPos = function(result, callback) {
       chessY = Math.round((minChessY + maxChessY) / 2);
       logger.debug("Chess scan finish", chessX, chessY);
 
-      // drawPosition(im, chessX, chessY, 0x00000000, function(im) {
-        callback(null);
-      // });
+      callback(null, {x: chessX, y: chessY});
     }
   });
 };
 
 let getBoardPos = function(result, callback) {
-  let im = result.takeScreenShot;
+  let im = result.screenShot;
 
   let scanEndX = im.bitmap.width;
   let scanEndY = im.bitmap.height * 2 / 3;
 
-  let backColor = Jimp.intToRGBA(im.getPixelColor(0, 0));
+  let backColor = Jimp.intToRGBA(im.getPixelColor(1, 1));
 
   let topX = 0
   let topY = 0;
+
+  let borderX = 0;
+  let borderY = 0;
 
   im.scan(0, 400, scanEndX, scanEndY, function(x, y, idx) {
     let red = this.bitmap.data[idx + 0];
     let green = this.bitmap.data[idx + 1];
     let blue = this.bitmap.data[idx + 2];
 
-    if (Math.abs(red - backColor.r) + Math.abs(green - backColor.g) + Math.abs(blue - backColor.b) > 10) {
+    if (Math.abs(red - backColor.r) + Math.abs(green - backColor.g) + Math.abs(blue - backColor.b) > 50) {
       if (!topY) {
         topY = y;
         topX = x;
@@ -135,25 +146,60 @@ let getBoardPos = function(result, callback) {
     }
 
     if (x == scanEndX - 1 && y == scanEndY - 1) {
-      topX = Math.round(topX);
-      topY = Math.round(topY);
+      borderX = Math.round(topX);
+      borderY = Math.round(result.chessPos.y - Math.abs(borderX - result.chessPos.x) * Math.sqrt(3) / 3);
 
-      logger.debug("Border scan finish", topX, topY);
+      logger.debug("Border scan finish", borderX, borderY);
 
-      drawPosition(im, topX, topY, 0x00000000, function(im) {
-        callback(null);
-      });
+      callback(null, {x: borderX, y: borderY});
     }
   });
 };
 
-let drawPosition = function(im, drawX, drawY, color, callback) {
-  logger.debug("Draw", drawX, drawY);
+// 在截图中画出定位点，以便后期debug
+let drawLine = function(result, callback) {
+  let im = result.screenShot;
 
+  async.series({
+    drawChess: function(cb) {
+      drawPosition(im, result.chessPos.x, result.chessPos.y, 0x00000000, function(im) {
+        cb(null);
+      });
+    },
+    drawBoard: function(cb) {
+      drawPosition(im, result.boardPos.x, result.boardPos.y, 0x00000000, function(im) {
+        cb(null);
+      });
+    }
+  }, function(err, result) {
+    let filename = moment().format("HHmmssSSS") + "_d.png";
+
+    im.write(screenShotDir+ "/" + filename);
+    callback(null);
+  })
+};
+
+// 根据棋子和方块位置算出按下屏幕的时间
+let jump = function(result, callback) {
+  let distance = Math.sqrt(Math.pow((result.chessPos.x - result.boardPos.x), 2) + Math.pow((result.chessPos.x - result.boardPos.x), 2));
+  
+  let pressTime = distance * deviceConfig.pressCoefficient / 1000;
+
+  logger.debug('Distance', distance);
+  logger.debug('PressTime', pressTime);
+
+  c.session().then(s => {
+    s.tapHold(200, 200, pressTime);
+  });
+
+  callback(null);
+}
+
+// 画线
+let drawPosition = function(im, drawX, drawY, color, callback) {
   im.scan(0, 0, im.bitmap.width, im.bitmap.height, function(x, y, idx) {
     if (x == drawX || y == drawY) {
       im.setPixelColor(color, x, y);
-      // logger.debug(x, y);
     }
 
     if (x == im.bitmap.width - 1 && y == im.bitmap.height - 1) {
@@ -162,19 +208,22 @@ let drawPosition = function(im, drawX, drawY, color, callback) {
   });
 };
 
-async.auto(
-  {
-    takeScreenShot,
-    getChessPos: ["takeScreenShot", getChessPos],
-    getBoardPos: ["takeScreenShot", getBoardPos]
-  },
-  function(err, result) {
-    if (err) {
-      logger.error(err);
+setInterval(function() {
+  async.auto(
+    {
+      screenShot: takeScreenShot,
+      chessPos: ["screenShot", getChessPos],
+      boardPos: ["chessPos", getBoardPos],
+      jump: ["chessPos", "boardPos", jump],
+      drawLine: ["chessPos", "boardPos", drawLine]
+    },
+    function(err, result) {
+      if (err) {
+        return logger.error(err);
+      }
+  
+      logger.debug("success");
     }
+  );
+}, 5000);
 
-    result.takeScreenShot.write('./test2.png');
-
-    logger.debug("success");
-  }
-);
